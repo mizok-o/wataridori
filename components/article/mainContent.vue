425cdd49506ba00001c9caf32cfb30603548603e
mizok-o <mizoko320@gmail.com>
2020-09-09 19:22:16 +0900

    一旦コミット

---
+++ components/article/mainContent.vue
@@ -0,0 +1,28 @@
+<template lang="pug">
+.p-article__content-container
+  partsInfo(:content="content")
+  partsHeading(:content="content")
+  partsLead(:content="content")
+  partsBody(:content="content")
+</template>
+<script>
+import partsInfo from '~/components/article/parts/partsInfo.vue'
+import partsBody from '~/components/article/parts/partsBody.vue'
+import partsHeading from '~/components/article/parts/partsHeading.vue'
+import partsLead from '~/components/article/parts/partsLead.vue'
+export default {
+  props: ['content'],
+  components: {
+    partsInfo,
+    partsBody,
+    partsHeading,
+    partsLead
+  }
+}
+</script>
+<style lang="sass">
+.p-article__content-container
+  margin-top: 48px
+  +sp-view
+    margin-top: 24px
+</style>
