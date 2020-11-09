315b7ca95be88f51e3e938d8c948706e7631662b
mizok-o <mizoko320@gmail.com>
2020-11-10 01:00:39 +0900

    フッターを作る

--- components/layouts/pfooter.vue
+++ components/layouts/pfooter.vue
@@ -1,16 +1,55 @@
 <template lang="pug">
-footer
-  p copyright by wataridori
+footer.footer
+  .footer__container
+    .footer__main-content
+      ul.footer__left
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+      ul.footer__right
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+        li.footer__main-item
+          a(href="/") プライバシーボリシー
+    p.footer__copy-text All copyright deserved by Kotaro Mizoguchi
 </template>
 <style lang="sass">
-footer
-  width: 112px
-  margin: 112px auto 32px
-  color: #222222
+.footer
+  width: 100%
+  height: 200px
+  margin: 112px 0 0
+  background-color: #eeeeee
   +sp-view
     margin-top: 56px
-  p
-    font-size: 10px
-    opacity: .5
-    white-space: nowrap
+
+.footer__container
+  padding: 32px 0
+
+.footer__main-content
+  display: flex
+  justify-content: center
+  margin: 0 auto
+  background-color: #ccccff
+
+.footer__main-item
+  margin-top: 12px
+  &:first-child
+    margin: 0
+
+.footer__copy-text
+  margin-top: 24px
+  font-size: 10px
+  opacity: .5
+  text-align: center
+  white-space: nowrap
+  background-color: #aaaaaa
 </style>
